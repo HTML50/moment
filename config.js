721daf269b9a2a86937d44478a8dd45f0d7085e1
gd <whoisfrank@foxmail.com>
2017-04-21 09:31:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var _config = {
-    appName       : '此刻',          // 博客名称
-    githubId       : 'HTML50',       // github 用户名
+    appName       : '湖畔',          // 博客名称
+    githubId       : 'k2pt',       // github 用户名
     repo            : 'moment',       // github 仓库名
     backgroundMusic      : 'http://m2.music.126.net/TZZspObsgil8RT2Oy9pErg==/2073678929995034.mp3',          
     // 背景音乐的绝对地址
