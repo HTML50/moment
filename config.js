761222629275134403ccee4effdfbef11f16620c
2Byte <iambenf@gmail.com>
2017-06-17 21:43:36 +0800

    update bgm url

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var _config = {
     appName       : '此刻',          // 博客名称
     githubId       : 'HTML50',       // github 用户名
     repo            : 'moment',       // github 仓库名
-    backgroundMusic      : 'http://m2.music.126.net/TZZspObsgil8RT2Oy9pErg==/2073678929995034.mp3',          
+    backgroundMusic      : 'http://119.90.25.30/m7.music.126.net/20170617220607/254c893b19058575765bc9f3f6c22019/ymusic/1caf/962e/ecd2/0d1f1e06f20e3a888ff2e672d1643999.mp3',          
     // 背景音乐的绝对地址
     // access_token : 'abcde'+'fghijk',       
     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
