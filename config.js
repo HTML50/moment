cde054325e816d50dd68611c4b29521ceae96d99
2Byte <iambenf@gmail.com>
2017-06-21 15:40:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var _config = {
     appName       : '此刻',          // 博客名称
     githubId       : 'HTML50',       // github 用户名
     repo            : 'moment',       // github 仓库名
-    backgroundMusic      : 'https://m8.music.126.net/20170620171650/3ff0de4559d8f910d78991a4f56c1dd7/ymusic/1caf/962e/ecd2/0d1f1e06f20e3a888ff2e672d1643999.mp3',          
+    backgroundMusic      : 'http://other.web.ra01.sycdn.kuwo.cn/resource/n2/128/12/69/4223213105.mp3',          
     // 背景音乐的绝对地址
     // access_token : 'abcde'+'fghijk',       
     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
