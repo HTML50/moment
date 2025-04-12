620c22134fd34f1b5556c0c3f13a466758301c3d
2byte <iambenf@gmail.com>
2025-04-12 22:20:20 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -3,6 +3,7 @@
     githubId       : 'HTML50',       // github 用户名
     repo            : 'moment',       // github 仓库名
     backgroundMusic      : '//music.163.com/song/media/outer/url?id=1500817509.mp3',
+    defaultImage  :'//s21.ax1x.com/2025/04/12/pERIFSA.jpg',
     // 背景音乐的绝对地址
     // access_token : 'abcde'+'fghijk',       
     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
